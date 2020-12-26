6d518397b5bd81591620e9c06555e88e5456e816
KateChoo <twodoor1993@gmail.com>
2020-12-26 10:53:32 +0800

    initial too

--- octagonal.js
+++ octagonal.js
@@ -8,42 +8,42 @@ function doFirst(){
     context.strokeStyle = 'gold';
     context.fillStyle='blue';
     context.lineWidth = 5;
-    //right
+    
     context.moveTo(510, 260);
-    context.lineTo(590, 160); //+80 -100
-    context.lineTo(570, 290); //+60 +30
-    context.lineTo(710, 260); //+200 0
-    context.lineTo(590, 360); //+80
-    context.lineTo(710, 440); //+220
-    context.lineTo(550, 430); //+60
-    context.lineTo(590, 560); //+100 
-    context.lineTo(490, 460); //0
+    context.lineTo(590, 160);  
+    context.lineTo(570, 290); 
+    context.lineTo(710, 260); 
+    context.lineTo(590, 360); 
+    context.lineTo(710, 440); 
+    context.lineTo(550, 430); 
+    context.lineTo(590, 560); 
+    context.lineTo(490, 460);
     
     context.beginPath();
     context.strokeStyle = 'gold';
     context.fillStyle='blue';
     context.lineWidth = 5;
-    //right
-    context.moveTo(510, 260);
-    context.lineTo(590, 160); //+80 -100
-    context.lineTo(570, 290); //+60 +30
-    context.lineTo(710, 260); //+200 0
-    context.lineTo(590, 360); //+80
-    context.lineTo(730, 440); //+220
-    context.lineTo(570, 430); //+60
-    context.lineTo(610, 560); //+100 
-    context.lineTo(510, 460); //0
     
-    //left
+    context.moveTo(510, 260);
+    context.lineTo(590, 160); 
+    context.lineTo(570, 290); 
+    context.lineTo(710, 260);
+    context.lineTo(590, 360); 
+    context.lineTo(730, 440); 
+    context.lineTo(570, 430); 
+    context.lineTo(610, 560); 
+    context.lineTo(510, 460);
+    
+    
     context.moveTo(510, 250);
-    context.lineTo(430, 150); //-80 -100
-    context.lineTo(450, 280); //-60 +30
-    context.lineTo(310, 250); //-200 0
-    context.lineTo(430, 350); //-80
-    context.lineTo(290, 430); //-220
-    context.lineTo(450, 420); //-60
-    context.lineTo(410, 550); //-100
-    context.lineTo(510, 450); //0
+    context.lineTo(430, 150); 
+    context.lineTo(450, 280); 
+    context.lineTo(310, 250); 
+    context.lineTo(430, 350); 
+    context.lineTo(290, 430); 
+    context.lineTo(450, 420); 
+    context.lineTo(410, 550);
+    context.lineTo(510, 450);
 
     context.closePath();
     context.stroke();
@@ -61,7 +61,7 @@ function doFirst(){
     context.strokeStyle = 'gold';
     context.fillStyle='red';
     context.lineWidth = 5;
-    //right
+    
     context.moveTo(500, 250);
     context.lineTo(580, 150); //+80 -100
     context.lineTo(560, 280); //+60 +30
@@ -72,7 +72,7 @@ function doFirst(){
     context.lineTo(600, 550); //+100 
     context.lineTo(500, 450); //0
     
-    //left
+    
     context.moveTo(500, 250);
     context.lineTo(420, 150); //-80 -100
     context.lineTo(440, 280); //-60 +30
